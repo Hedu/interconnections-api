1d2eb3a69b777140ea062e374524b81cc53e5c75
hedu <eperezghedu@gmail.com>
2017-03-06 01:07:28 +0100

    comment print and call service

--- src/main/java/com/hedu/ryanair/interconnections/controller/InterconnectionController.java
+++ src/main/java/com/hedu/ryanair/interconnections/controller/InterconnectionController.java
@@ -1,9 +1,12 @@
 package com.hedu.ryanair.interconnections.controller;
 
 import com.hedu.ryanair.interconnections.model.Interconnection;
+import com.hedu.ryanair.interconnections.util.InterconnectionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by hedu on 5/03/17.
@@ -13,17 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class InterconnectionController {
 
     @GetMapping("/interconnections")
-    public Interconnection interconnection(
+    public List<Interconnection> interconnection(
             @RequestParam(value="departure") String departure,
             @RequestParam(value="arrival") String arrival,
             @RequestParam(value="departureDateTime") String departureDateTime,
             @RequestParam(value="arrivalDateTime") String arrivalDateTime) {
 
-        System.out.println("departure: " + departure);
+        /*System.out.println("departure: " + departure);
         System.out.println("arrival: " + arrival);
         System.out.println("departureDateTime: " + departureDateTime);
-        System.out.println("arrivalDateTime: " + arrivalDateTime);
+        System.out.println("arrivalDateTime: " + arrivalDateTime);*/
 
-        return null;
+        return InterconnectionService.getInterconnections(departure, arrival, departureDateTime, arrivalDateTime);
     }
 }
