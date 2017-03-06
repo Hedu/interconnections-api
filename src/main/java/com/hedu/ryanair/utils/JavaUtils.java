3922db85159c84cf969d4c27864a341c782873fe
hedu <eperezghedu@gmail.com>
2017-03-06 01:04:04 +0100

    add new conversion utils

--- src/main/java/com/hedu/ryanair/utils/JavaUtils.java
+++ src/main/java/com/hedu/ryanair/utils/JavaUtils.java
@@ -1,18 +1,33 @@
 package com.hedu.ryanair.utils;
 
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 /**
  * Created by hedu on 5/03/17.
  */
 public class JavaUtils {
-    public static LocalDate getISOTime(String str) {
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-ddTHH:mm");
-        LocalDate dt = LocalDate.parse(str, formatter);
+    public static LocalDateTime getISOTime(String str) {
+        str = str.replace('T', ' ');
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime dt = LocalDateTime.parse(str, formatter);
 
         return dt;
+    }
+
+    public static  String getISOTime(LocalDateTime localTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String formatDateTime = localTime.format(formatter);
+
+        return formatDateTime.replace(' ', 'T');
+    }
+
+    public static LocalTime getTime(String str) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime localTime = LocalTime.parse(str, formatter);
+
+        return localTime;
     }
 }
