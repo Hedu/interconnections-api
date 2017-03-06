7204bcebbc205ef524355a37d240f22524aa2802
hedu <eperezghedu@gmail.com>
2017-03-06 01:08:08 +0100

    add constructor

--- src/main/java/com/hedu/ryanair/interconnections/model/DefaultInterconnection.java
+++ src/main/java/com/hedu/ryanair/interconnections/model/DefaultInterconnection.java
@@ -1,8 +1,14 @@
 package com.hedu.ryanair.interconnections.model;
 
+import java.util.List;
+
 /**
  * Created by hedu on 5/03/17.
  */
 public class DefaultInterconnection extends Interconnection{
 
+    public DefaultInterconnection(int stops, List<Leg> legs) {
+        this.stops = stops;
+        this.legs = legs;
+    }
 }
